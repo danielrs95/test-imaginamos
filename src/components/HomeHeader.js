896ad23cd8cb621679b76b3e1cb462686422abee
Daniel <danielrs9504@gmail.com>
2021-09-14 21:34:54 -0500

    Agrega carpeta assets con imagenes, iconos y jsons Crea el HomeHeader basico con la imagen

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import headerImg from '../assets/images/headerimage.png';
 
 const HomeHeader = () => {
   return (
@@ -16,22 +17,12 @@ const HomeHeader = () => {
             <div
               className='card-header container d-flex bg-gradient justify-content-evenly align-items-center'
               style={{
-                backgroundColor: '#2196f3',
-                borderRadius: '1.2rem 1.2rem 0 0',
+                backgroundColor: '#fff7eb',
               }}
             >
               <div className='row'>
                 <div className='col-12 col-lg-6 d-flex justify-content-center'>
-                  <img
-                    src='..'
-                    className='img-fluid w-50'
-                    alt='...'
-                    style={{
-                      border: '0.1px solid rgb(0 0 0 / 10%)',
-                      borderRadius: '1.2rem',
-                      boxShadow: '5px 5px 1.5px 0 rgb(0 0 0 / 10%)',
-                    }}
-                  />
+                  <img src={headerImg} className='img-fluid ' alt='...' />
                 </div>
 
                 <div className='d-flex flex-column col-12 col-lg-6 text-center text-light justify-content-center'>
