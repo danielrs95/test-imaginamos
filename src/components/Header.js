a74d51dbd29585300f74c89f57cb33c1aa73760c
Daniel <danielrs9504@gmail.com>
2021-09-14 20:58:57 -0500

    Agrega layout basico en el navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container'>
+        <a className='navbar-brand' href='#'>
+          Navbar
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div
+          className='collapse navbar-collapse ms-5'
+          id='navbarSupportedContent'
+        >
+          <form className='d-flex ms-auto'>
+            <input
+              className='form-control me-2'
+              type='search'
+              placeholder='Search'
+              aria-label='Search'
+            />
+            <button className='btn btn-outline-success' type='submit'>
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
