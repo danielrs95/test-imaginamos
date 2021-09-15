af64aef6609d6155203b9e7619fe4ce06bd71ac5
Daniel <danielrs9504@gmail.com>
2021-09-14 21:56:43 -0500

    Simplifica codigo del header

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -5,29 +5,26 @@ const HomeHeader = () => {
   return (
     <div className='container mt-4'>
       <div className='row justify-content-center'>
-        <div className='col-11' style={{ borderRadius: '10rem' }}>
-          <div
-            className='container d-flex justify-content-evenly align-items-center'
-            style={{
-              backgroundColor: '#fff7eb',
-              borderRadius: '5rem',
-              height: '90%',
-            }}
-          >
-            <div className='row'>
-              <div className='col-12 col-lg-4  justify-content-center'>
-                <img src={headerImg} className='img-fluid ' alt='...' />
-              </div>
+        <div
+          className='col-12 container d-flex justify-content-evenly'
+          style={{
+            backgroundColor: '#fff7eb',
+            borderRadius: '5rem',
+          }}
+        >
+          <div className='row'>
+            <div className='col-12 col-lg-4  justify-content-center'>
+              <img src={headerImg} className='img-fluid ' alt='...' />
+            </div>
 
-              <div className='d-flex flex-column col-12 col-lg-6 text-center text-light justify-content-center'>
-                <h1 className='card-text fw-bold text-primary '>
-                  Daniel Ramirez
-                </h1>
-              </div>
+            <div className='d-flex flex-column col-12 col-lg-6 text-center text-light justify-content-center'>
+              <h1 className='card-text fw-bold text-primary '>
+                Daniel Ramirez
+              </h1>
+            </div>
 
-              <div className='col-lg-2 d-flex flex-column justify-content-end'>
-                Learn more
-              </div>
+            <div className='col-lg-2 d-flex flex-column justify-content-end'>
+              Learn more
             </div>
           </div>
         </div>
