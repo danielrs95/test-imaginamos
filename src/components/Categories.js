c750147d302eb8e384781b2d8a7204a33d05055a
Daniel <danielrs9504@gmail.com>
2021-09-15 22:10:20 -0500

    Agrega estilos a las categorias

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,12 +4,54 @@ import Category from './Category';
 
 const Categories = () => {
   return (
-    <div className='container'>
+    <div className='container mt-4'>
       <div className='row'>
         <div className='col-12'>
-          <div className='d-flex'>
-            <span>Restaurantes</span>
-            <span>Boton delivery</span>
+          <div className='d-flex justify-content-between'>
+            <div className='d-flex align-items-center'>
+              <h1>Restaurantes</h1>
+              <img
+                src='/icons/1046784.svg'
+                className='p-2'
+                alt='...'
+                style={{ width: '2.5rem' }}
+              />
+            </div>
+            <div class='btn-group'>
+              <button
+                type='button'
+                class='btn dropdown-toggle text-light px-5 rounded-pill'
+                data-bs-toggle='dropdown'
+                aria-expanded='false'
+                style={{ backgroundColor: '#ff6223' }}
+              >
+                <i class='far fa-clock'></i>
+                <span className='mx-2'>Delivery: </span>
+                <strong>Now</strong>
+              </button>
+              <ul class='dropdown-menu'>
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Later
+                  </a>
+                </li>
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Something else here
+                  </a>
+                </li>
+                <li>
+                  <a class='dropdown-item' href='#'>
+                    Separated link
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         {categories.map((category) => (
--- src/components/Category.js
+++ src/components/Category.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Category = ({ category }) => {
   return (
-    <button className='btn btn-outline-warning d-flex align-items-center flex-column col-1 border rounded-pill mx-2'>
+    <button className='btn btn-outline-warning d-flex align-items-center flex-column col-1 border rounded-pill mx-2 mt-4'>
       <img
         src={category.icon}
         className='img-fluid border rounded-circle p-2 mt-3 bg-light'
