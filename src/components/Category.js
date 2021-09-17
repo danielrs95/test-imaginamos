46ba2f01f186e818478577bd6d1ee972d9260bde
Daniel <danielrs9504@gmail.com>
2021-09-16 19:40:33 -0500

    Agrega responsive a las categorias

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Category = ({ category }) => {
   return (
-    <button className='btn btn-outline-warning d-flex align-items-center flex-column col-1 border rounded-pill mx-2 mt-4'>
-      <img
-        src={category.icon}
-        className='img-fluid border rounded-circle p-2 mt-3 bg-light'
-        alt='...'
-      />
-      <div className='mt-3 mb-5 text-dark text-center'>{category.name}</div>
-    </button>
+    <div className='col-3 d-flex flex-column col-lg-2 col-xxl-1'>
+      <button className=' btn btn-outline-warning  align-items-center   border rounded-pill mt-4'>
+        <img
+          src={category.icon}
+          className='img-fluid border rounded-circle p-2 mt-3 bg-light'
+          alt='...'
+        />
+        <div className='mt-3 mb-5 text-dark text-center'>{category.name}</div>
+      </button>
+    </div>
   );
 };
 
