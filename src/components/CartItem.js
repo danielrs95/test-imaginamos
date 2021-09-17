0138683b0ef6b3472050a736f84311f2fcea24b3
Daniel <danielrs9504@gmail.com>
2021-09-16 20:50:12 -0500

    Finaliza el responsive del carrito de compra

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,19 +3,21 @@ import React from 'react';
 const CartItem = ({ item }) => {
   return (
     <>
-      <div className='col-12 col-xxl-6'>
-        <img
-          src={item.image}
-          className='img-fluid bg-light'
-          alt='...'
-          style={{ borderRadius: ' 1rem' }}
-        />
-      </div>
-      <div className='col-12 d-lg-flex align-items-lg-center justify-content-lg-between col-xxl-6 text-center '>
-        <span className='fw-bold'>
-          {item.qty} x {item.name}
-        </span>
-        <span className='text-muted'>${item.price}</span>
+      <div className='d-xxl-flex'>
+        <div className='col-12 col-xxl-6'>
+          <img
+            src={item.image}
+            className='img-fluid bg-light my-2'
+            alt='...'
+            style={{ borderRadius: ' 1rem' }}
+          />
+        </div>
+        <div className='col-12 d-lg-flex align-items-lg-center justify-content-lg-between col-xxl-6 text-center '>
+          <span className='fw-bold'>
+            {item.qty} x {item.name}
+          </span>
+          <span className='text-muted'>${item.price}</span>
+        </div>
       </div>
     </>
   );
